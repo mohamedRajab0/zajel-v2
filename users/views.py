2d787aa6164cf1d6e4525f153c2d1638f51ee7aa
aalaa <alaamglal@gmail.com>
2024-10-18 18:01:15 +0300

    edit views for api

--- users/views.py
+++ users/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
-
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from rest_framework.authentication import TokenAuthentication
 
-from .forms import ProfileForm
+#from .forms import ProfileForm
 from django.contrib.auth.views import redirect_to_login
 from .models import *
 from django.urls import reverse
@@ -14,29 +12,6 @@ from rest_framework import permissions
 from .serializers import ProfileSerializer, UserSerializer
 from  .models import Profile
 
-# Create your views here
-
-class ProfileViewSet(viewsets.ModelViewSet):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated]
-    queryset = User.objects.all()
-    serializer_class = UserSerializerfrom django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
-from rest_framework.authentication import TokenAuthentication
-
-from .forms import ProfileForm
-from django.contrib.auth.views import redirect_to_login
-from .models import *
-from django.urls import reverse
-from rest_framework import viewsets
-from rest_framework import permissions
-from .serializers import ProfileSerializer, UserSerializer
-from  .models import Profile
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
