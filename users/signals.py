041c190864e7fa96d316e2278799cfd4608cfe43
aalaa <alaamglal@gmail.com>
2024-10-18 18:44:14 +0300

    add profile if created

---
+++ users/signals.py
@@ -0,0 +1,31 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from allauth.account.models import EmailAddress
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def user_postsave(sender, instance, created, **kwargs):
+    user = instance
+
+    print("is my post save working ?")
+    # add profile if user is created
+    if created:
+        print("Creating a profile for the user:", user)
+        Profile.objects.create(user=user)
+        print("Profile created:", user.profile)
+    # else:
+    #     # update allauth emailaddress if exists
+    #     print("user already")
+    #     try:
+    #         email_address = EmailAddress.objects.get_primary(user)
+    #         if email_address.email != user.email:
+    #             email_address.email = user.email
+    #             email_address.verified = False
+    #             email_address.save()
+    #     except:
+    #         # if allauth emailaddress doesn't exist create one
+    #         EmailAddress.objects.create(
+    #             user=user, email=user.email, primary=True, verified=False
+    #         )
---
+++ users/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from users.views import *
+from  rest_framework.routers import DefaultRouter
+from dj_rest_auth.views import (
+    LoginView, LogoutView, PasswordChangeView, PasswordResetConfirmView,
+    PasswordResetView, UserDetailsView,
+)
+from dj_rest_auth.registration.views import RegisterView
+
+app_name = 'users'
+
+router = DefaultRouter()
+router.register(r'profile', ProfileViewSet, basename='profile')
+router.register(r'user', UserViewSet, basename='user')
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+    path('signup/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('user/', UserDetailsView.as_view(), name='user')
+]
