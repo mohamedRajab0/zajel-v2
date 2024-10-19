94bd23e1cc1c922848a81de168eb9f72c601aa81
abdallhfattah <abdallh.fattah33@gmail.com>
2024-10-19 17:20:26 +0300

    creating a Profile for each registered user using signals.py

--- a_users/signals.py
+++ a_users/signals.py
@@ -8,7 +8,6 @@ from .models import Profile
 @receiver(post_save, sender=User)
 def user_postsave(sender, instance, created, **kwargs):
     user = instance
-
     # add profile if user is created
     if created:
         Profile.objects.create(user=user)
