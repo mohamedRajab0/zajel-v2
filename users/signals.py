fcf5c6b1e0c1469a6c14fa17cc130e75d832b329
abdallhfattah <abdallh.fattah33@gmail.com>
2024-10-19 16:40:53 +0300

    took unnecessary prints

--- users/signals.py
+++ users/signals.py
@@ -8,16 +8,9 @@ from .models import Profile
 @receiver(post_save, sender=User)
 def user_postsave(sender, instance, created, **kwargs):
     user = instance
-
-    print("is my post save working ?")
-    # add profile if user is created
     if created:
-        print("Creating a profile for the user:", user)
         Profile.objects.create(user=user)
-        print("Profile created:", user.profile)
     else:
-        # update allauth emailaddress if exists
-        print("user already")
         try:
             email_address = EmailAddress.objects.get_primary(user)
             if email_address.email != user.email:
@@ -25,7 +18,6 @@ def user_postsave(sender, instance, created, **kwargs):
                 email_address.verified = False
                 email_address.save()
         except:
-            # if allauth emailaddress doesn't exist create one
             EmailAddress.objects.create(
                 user=user, email=user.email, primary=True, verified=False
             )
