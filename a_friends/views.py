45132ed9967ab57b284a4f00e7d323a2b6107437
mohamedRajab0 <elzaimohamed@gmail.com>
2024-10-20 06:44:37 +0300

    Add FriendRequestViewSet with authentication check in perform_create

--- a_friends/views.py
+++ a_friends/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.exceptions import ValidationError
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .models import FriendList, FriendRequest
+from .serializers import FriendRequestSerializer, FriendListSerializer
 
-# Create your views here.
+
+# ViewSet for Friend Requests
+class FriendRequestViewSet(viewsets.ModelViewSet):
+    queryset = FriendRequest.objects.all()
+    serializer_class = FriendRequestSerializer
+
+    def perform_create(self, serializer):
+        sender = self.request.user
+        if not sender.is_authenticated:
+            raise ValidationError({"error": "Authentication required"}, code=status.HTTP_401_UNAUTHORIZED)
+        serializer.save(sender=sender)
