47b4655fcafb70433630ffbd79efed06a72fb71a
abdallhfattah <abdallh.fattah33@gmail.com>
2024-10-22 13:47:08 +0300

    added a modelViewSet for ZajelGroup model

--- a_chat/views.py
+++ a_chat/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import ZajelGroup
+from .serializers import  ZajelGroupSerializer
 
-# Create your views here.
+class ZajelGroupViewSet(viewsets.ModelViewSet):
+    queryset = ZajelGroup.objects.filter()
+    serializer_class = ZajelGroupSerializer
+
+    def list(self, request, *args, **kwargs):
+        user = self.request.user
+        # Filter groups where the user is a member
+        groups_this_user_is_in = ZajelGroup.objects.filter(members=user)
+
+        # Debug print to see the groups
+        print(groups_this_user_is_in)
+
+        # Update the queryset to only include these groups
+        self.queryset = groups_this_user_is_in
+
+        return super().list(request, *args, **kwargs)
