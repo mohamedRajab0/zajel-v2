fcef66b8abc8c4dffbcebb3d1155f4281e81074a
mohamedRajab0 <elzaimmohamed@gmail.com>
2024-11-04 16:12:12 +0200

    import create.css

--- frontend/src/Create.jsx
+++ frontend/src/Create.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./Create.css";
+
 
 function CreateGroupButton() {
   const [showDropdown, setShowDropdown] = useState(false);
