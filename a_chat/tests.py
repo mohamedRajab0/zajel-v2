4294d3ee6b4aa3b0185cf507e49e8e879edc8319
mohamedRajab0 <elzaimmohamed@gmail.com>
2024-11-04 06:55:20 +0200

    add unit test to zajel group model

--- a_chat/tests.py
+++ a_chat/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
-
+from .models import *
 # Create your tests here.
+
+
+class ZajelGroupTest(TestCase):
+    def setUp(self):
+        self.group = ZajelGroup.objects.create(group_name='test_group')
+        self.user = User.objects.create(username='test_user')
+        self.group.members.add(self.user)
+
+    def test_group_name(self):
+        self.assertEqual(self.group.group_name, 'test_group')
+
+    def test_group_members(self):
+        self.assertEqual(self.group.members.first(), self.user)
+
+    def test_group_image(self):
+        self.assertEqual(self.group.image, None)
+
+    def test_group_description(self):
+        self.assertEqual(self.group.description, None)
+
+    def test_group_created(self):
+        self.assertIsNotNone(self.group.created)
+
+    def test_group_name_property(self):
+        self.assertEqual(self.group.name, 'test_group')
