a0a0f7e99927aa1b9c4e07fd0397089741f418a7
mohamedRajab0 <elzaimmohamed@gmail.com>
2024-11-04 17:01:00 +0200

    ovrite create in zajelgroup add the login user to grou

--- a_chat/serializers.py
+++ a_chat/serializers.py
@@ -1,17 +1,27 @@
 from rest_framework import serializers
 from .models import ZajelMessage, ZajelGroup
 
+
 class ZajelMessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ZajelMessage
-        fields = '__all__'  # Or specify the fields you want to include in your API.
-        read_only_fields = ['group_name', 'author']  # Make these fields read-only
+        # Or specify the fields you want to include in your API.
+        fields = '__all__'
+        # Make these fields read-only
+        read_only_fields = ['group_name', 'author']
 
 
 class ZajelGroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = ZajelGroup
-        fields = '__all__'  # Or specify the fields you want to include in your API.
-        read_only_fields = ['members']
+        # Or specify the fields you want to include in your API.
+        fields = ['id', 'group_name', 'members', 'image', 'description']
 
+        def create(self, validated_data):
+            # remove members from validate data
+            members_data = validated_data.pop('members', [])
+            group = ZajelGroup.objects.create(**validated_data)
 
+            # add members if validated
+            group.members.set(members_data)
+            return group
